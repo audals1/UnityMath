1b1e1bbd2016b54fcccccb9885a3a1b088458329
audals1 <audals3@gmail.com>
2023-08-16 08:22:09 +0900

    230816

--- Chapter1/Assets/Scripts/DegreeComponent.cs
+++ Chapter1/Assets/Scripts/DegreeComponent.cs
@@ -19,12 +19,19 @@ public class DegreeComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dTime += Time.deltaTime;
-        float delta = scalar * Mathf.Sin(dTime); //sin그래프 곡선은 늘었다 줄었다 다시 늘어나는 모양이므로 우리가 원하는 움직임 구현가능
+        degress += Time.deltaTime * 50f;
+        //float delta = scalar * Mathf.Sin(dTime); //sin그래프 곡선은 늘었다 줄었다 다시 늘어나는 모양이므로 우리가 원하는 움직임 구현가능
 
         //방향(x,y)
-        float x = delta * Mathf.Cos(degress);
-        float y = delta * Mathf.Sin(degress);
+        float x = scalar * Mathf.Cos(DegreesToRad(degress));
+        float y = scalar * Mathf.Sin(DegreesToRad(degress));
+
+        //x += scalar * Mathf.Cos(DegreesToRad(degress * 3)); //원래 원으로 그려져야할 것들이 x 값 추가로 더해짐으로써 다른 형태로 움직임
+
+
+        //각도를 라디언으로 변경안하고 그냥 쓰면 각이 제대로 안들어가서 이상하게 움직인다
+        //float x = delta * Mathf.Cos(degress);
+        //float y = delta * Mathf.Sin(degress);
 
         Vector3 dir = new Vector3(x, y, 0);
 
